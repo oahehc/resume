b3cdac7cbe6475d75c1ebd7ddebfa1863d598497
oahehc <oahehc@gmail.com>
2019-01-20 12:05:06 +0800

    add logrocket

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import LogRocket from 'logrocket'
+import setupLogRocketReact from 'logrocket-react'
 import App from './App'
 import StyleProvider from './styles/StyleProvider'
 import Context from './Context/Context'
+
+LogRocket.init('oahehc/resume-r1lhl')
+setupLogRocketReact(LogRocket)
 
 ReactDOM.render(
   <Context>
