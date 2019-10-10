0e694273bc19269876b8697f6471de25d3ab82f3
oahehc <oahehc@gmail.com>
2019-10-10 17:16:32 +0800

    Updates

---
+++ precache-manifest.bdaff61e05982ac0440ed53d3107a268.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "773871c2c30240fc0c0a6188b643e43a",
+    "url": "/resume/index.html"
+  },
+  {
+    "revision": "e2d424f6b36d9d4b1154",
+    "url": "/resume/static/css/2.0ea451ac.chunk.css"
+  },
+  {
+    "revision": "e2d424f6b36d9d4b1154",
+    "url": "/resume/static/js/2.d1143dd3.chunk.js"
+  },
+  {
+    "revision": "56360681b2ac143362e2",
+    "url": "/resume/static/js/main.40b77141.chunk.js"
+  },
+  {
+    "revision": "699f89d957187d0ac949",
+    "url": "/resume/static/js/runtime~main.30522d0f.js"
+  }
+]);
