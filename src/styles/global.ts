95ca8b10b6850d938b2b6b4bf94e960e61e0dbd3
oahehc <oahehc@gmail.com>
2020-08-11 07:08:02 +0800

    Feature: apply smooth scroll

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -18,6 +18,7 @@ export default createGlobalStyle`
     -webkit-font-smoothing: antialiased;
     -moz-osx-font-smoothing: grayscale;
     background-color: ${vars.white};
+    scroll-behavior: smooth;
   }
 
   @media print {
