758f054122432475dd56a5e5bdc40191193a1c24
oahehc <oahehc@gmail.com>
2023-01-17 21:02:44 +0900

    feat: remove browser support popup

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,10 @@
 import React from 'react'
-import BrowserNotification from './Components/BrowserNotification/BrowserNotification'
 import ErrorBoundary from './Components/ErrorBoundary/ErrorBoundary'
 import AppRouter from './AppRouter'
 
 const App: React.SFC = () => {
   return (
     <ErrorBoundary>
-      <BrowserNotification />
       <AppRouter />
     </ErrorBoundary>
   )
